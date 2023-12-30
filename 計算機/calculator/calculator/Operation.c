ff4a84bada7dae9abc18c10c248d96e1594a575a
SalmonLai <kevin9404180310@gmail.com>
2023-12-30 21:55:25 +0800

    12/30

--- 計算機/calculator/calculator/Operation.c
+++ 計算機/calculator/calculator/Operation.c
@@ -1,51 +1,40 @@
 #include <stdio.h>
 #include <math.h>
-
+#define PI 3.1415926
 
 int calculator() {
 	
-	char operator;
+	char operator[10];
 	double num1, num2, result;
-	printf("請輸入運算符(+, -, *, /, ^, sqrt): ");
-	scanf(" %c", &operator);
+	printf("請輸入運算符(+, -, *, /, ^, %%, sqrt,log, sin, cos, tan): ");
+	scanf(" %s", operator);
 
 	printf("請輸入兩個數字: ");
 	scanf("%lf %lf", &num1, &num2);
 
-	switch (operator) {
-	case '+':
-		result = num1 + num2;
-		break;
-	case '-':
-		result = num1 - num2;
-		break;
-	case '*':
-		result = num1 * num2;
-		break;
-	case '/':
-		if (num2 != 0) {
-			result = num1 / num2;
-		}
-		else {
-			printf("錯誤：除數不能為0\n");
-			return 1;
-		}
-		break;
-	case '^':
-		result = pow(num1, num2);
-		break;
-	case 's':
-		if (num1 >= 0) {
-			result = sqrt(num1);
-		}
-		else {
-			printf("錯誤：負數無法計算平方根\n");
-			return 1;
-		}
-		break;
-	default:
-		printf("錯誤：無效的運算符\n");
-		return 1;
+	if (strcmp(operator, "+") == 0) result = num1 + num2;
+	else if(strcmp(operator, "-") == 0) result = num1 - num2;
+	else if (strcmp(operator, "*") == 0) result = num1 * num2;
+	else if (strcmp(operator, "/") == 0) {
+		if (num1 != 0) result = num1 / num2;
+		else printf("錯誤：除數不能為0\n");
 	}
-	printf("結果: %.2lf %c %.2lf = %.2lf\n", num1, operator, num2, result);
+	else if (strcmp(operator, "%") == 0) result = (int)num1 % (int)num2;
+	else if (strcmp(operator, "^") == 0) result = pow(num1, num2);
+	else if (strcmp(operator, "sqrt") == 0) {
+		if (num1 >= 0) result = sqrt(num1);
+		else printf("錯誤：負數無法計算平方根\n");
+	}
+	else if (strcmp(operator, "log") == 0) result = log(num2) / log(num1);
+
+	else if (strcmp(operator, "sin") == 0) result = sin(num1 / 180 * PI);
+	else if (strcmp(operator, "cos") == 0) result = cos(num1 / 180 * PI);
+	else if (strcmp(operator, "tan") == 0) result = tan(num1 / 180 * PI);
+	else if (strcmp(operator, "asin") == 0) result = (asin(num1) / PI * 180);
+	else if (strcmp(operator, "acos") == 0) result = (acos(num1) / PI * 180);
+	else if (strcmp(operator, "atan") == 0) result = (atan(num1) / PI * 180);
+	else printf("錯誤：無效的運算符\n");
+	
+	printf("結果: %.2lf %s %.2lf = %.2lf\n", num1, operator, num2, result);
+	saveHistory(operator, num1, num2, result);
 }
