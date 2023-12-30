f9fe4f3e8b194bdf7a1bdbf38dea2778dd2cfc6d
zewei1222 <zeiwei1222@gmail.com>
2023-12-31 02:23:23 +0800

    dick

--- 計算機/calculator/calculator/main.c
+++ 計算機/calculator/calculator/main.c
@@ -1,48 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../calculator//history.h"
 
 #define MAX_HISTORY_LENGTH 100
 
-void saveHistory(char *operation, double num1, double num2, double result) {
-	FILE *fptr = fopen("history.txt", "a");
-	if (fptr == NULL) {
-		printf("無法打開或創建 history.txt 文件。\n");
-		return;
-	}
-
-	fprintf(fptr, "%.2lf %s %.2lf = %.2lf\n", num1, operation, num2, result);
-	fclose(fptr);
-}
-
-void clearHistory() {
-	FILE *fptr = fopen("history.txt", "w");
-	if (fptr == NULL) {
-		printf("無法打開或創建 history.txt 文件。\n");
-		return;
-	}
-
-	fclose(fptr);
-	printf("歷史記錄已清除。\n");
-}
-
-void displayHistory() {
-	system("cls");
-	FILE *fptr = fopen("history.txt", "r");
-	if (fptr == NULL) {
-		printf("歷史記錄為空。\n");
-		return;
-	}
-
-	printf("歷史記錄：\n");
-	char c;
-	while ((c = getc(fptr)) != EOF) {
-		putchar(c);
-	}
-	fclose(fptr);
-}
-
 int main() {
 	char option;
+	char operator;
+	double num1, num2, result;
 	do {
 		system("cls");
 		printf("\n選擇功能：\n");
@@ -55,9 +20,6 @@ int main() {
 
 		switch (option) {
 		case '1': {
-			char operator;
-			double num1, num2, result;
-
 			printf("請輸入運算符(+, -, *, /)：");
 			scanf(" %c", &operator);
 
@@ -90,10 +52,6 @@ int main() {
 					printf("錯誤：除數不能為0\n");
 				}
 				break;
-			case '!':
-
-			
-
 			default:
 				printf("錯誤：無效的運算符\n");
 				break;
