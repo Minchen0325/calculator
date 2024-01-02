3788bbad2321c4aec90773ab154e9d5997a4782f
Minchen0325 <minchenqs0325@gmail.com>
2024-01-02 13:59:04 +0800

    complete

--- 計算機/calculator/calculator/translate.c
+++ 計算機/calculator/calculator/translate.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include <math.h>
 
+int checkb2d(int x)
+{
 
+	int check, ret = 1;
+	while (x)
+	{
+		check = x % 10;
+		x = x / 10;
+
+		if (check != 0 && check != 1)
+		{
+			printf("輸入錯誤(只能0跟1\n)");
+			ret = 0;
+			x = 0;
+			break;
+		}
+	}
+	return ret;
+}
+
+int checkd2b(int x)
+{
+	int check, ret = 1;
+	while (x)
+	{
+		check = x % 2;
+		x = x / 2;
+		if (check != 0 && check != 1)
+		{
+			printf("輸入錯誤");
+			ret = 0;
+			x = 0;
+			break;
+		}
+	}
+	return ret;
+}
 
 int b2d(int x)
 {
@@ -43,32 +79,102 @@ int d2b(int x)
 
 int binary() {
 	char choose[10];
-	int x;
-	int back;
+	char x[20], a[20];
+	int sum = 0, count = -1, dontdo = 1;
+
+
 	do {
 		system("cls");
 		printf("1.二進制轉十進制\n");
 		printf("2.十進制轉二進制\n");
-		printf("3.退出\n");
+		printf("3.返回主選單\n");
 		printf("請選擇想要的功能:");
 		scanf("%s", choose);
-		if (strcmp(choose, "3") == 0) {
-			printf("成功退出\n");
-			break;
+
+
+
+		int doit = 0;
+
+		for (int i = 0; i < 10; i++)
+		{
+			doit = doit + checkdonkey(choose[i]);
+			if (doit)break;
 		}
-		while (strcmp(choose, "1") != 0 && strcmp(choose, "2") != 0 && strcmp(choose, "3") != 0) {
-			printf("輸入錯誤，請重新輸入。");
-			scanf("%s", choose);
-		}
-		if (strcmp(choose, "1") == 0) {
-			printf("請輸入二進制:");
-			scanf("%d", &x);
-			printf("十進制為:%d\n", b2d(x));
-		}
-		else if (strcmp(choose, "2") == 0) {
-			printf("請輸入十進制:");
-			scanf("%d", &x);
-			printf("十進制為:%d\n", d2b(x));
+
+		doit = doit - 2;
+
+		if (doit == 0)
+		{
+
+			if (strcmp(choose, "3") == 0) {
+				printf("成功退出\n");
+				break;
+			}
+			while (strcmp(choose, "1") != 0 && strcmp(choose, "2") != 0 && strcmp(choose, "3") != 0) {
+				printf("輸入錯誤，請重新輸入。");
+				scanf("%s", choose);
+			}
+
+
+			sum = 0, dontdo = 1, count = -1;//重複使用需重置
+
+			if (strcmp(choose, "1") == 0) {
+				printf("請輸入二進制:");
+				scanf("%s", x);
+
+				for (int i = 0; i < sizeof(x) / sizeof(x[0]); i++)//個別偵錯
+				{
+					count++;
+					if (checkdonkey(x[i]) == 1) { dontdo = 0; break; }
+					if (checkdonkey(x[i])) break;
+
+				}
+
+				for (int i = 0; i < count; i++)//char轉int
+				{
+					int rig = 1;
+					for (int j = i; j < count - 1; j++)
+					{
+						rig = rig * 10;
+					}
+					sum = sum + change(x[i])*rig;
+				}
+				if (checkb2d(sum) && dontdo)//輸出
+				{
+					printf("十進制為:%d\n", b2d(sum));
+				}
+			}
+
+			else if (strcmp(choose, "2") == 0) {
+				printf("請輸入十進制:");
+				scanf("%s", a);
+				
+				for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)//個別偵錯
+				{
+					count++;
+					if (checkdonkey(a[i]) == 1) { dontdo = 0; break; }
+					if (checkdonkey(a[i])) break;
+				}
+
+				for (int i = 0; i < count; i++)//char轉int
+				{
+					int rig = 1;
+					for (int j = i; j < count - 1; j++)
+					{
+						rig = rig * 10;
+					}
+					sum = sum + change(a[i])*rig;
+				}
+
+
+				if (checkd2b(sum) && dontdo)//輸出
+				{
+					printf("二進制為:%d\n", d2b(sum));
+				}
+				
+				printf("%b", (int)a);
+			}
+
 		}
 		system("pause");
 	} while (strcmp(choose, "3") != 0);
--- 計算機/calculator/calculator/translate.h
+++ 計算機/calculator/calculator/translate.h
@@ -1,3 +1,5 @@
 int d2b(double x) {}
 double b2d(int x) {}
-void binary(){}
+void binary() {}
+int checkb2d(int x) {}
+int checkd2b(int x) {}
